acbd33a400e36355a52a26adea88795e63291af8
schweitzerp <schweitzer.philip@gmail.com>
2019-02-22 14:38:28 -0500

    Friday's work midway

--- pixel.sol
+++ pixel.sol
@@ -1,7 +1,36 @@
-pragma solidity ^0.4.25
+pragma solidity ^0.4.25;
 
-import "./ownable.sol"
+import "./ownable.sol";
+import "./safemath.sol";
 
-contract pixel is ownable{
-	
+contract Plethera is Ownable {
+  
+  //each Pixel object stores it's linear location and color in 3 byte RGB
+  struct Pixel{
+  	bytes4 color;
+  	uint32 _pixelNum
+  }
+
+  //the whole picture object is an array of these pixels and is called picture
+  Pixel[] public picture;
+
+  mapping (uint => address) public pixelToOwner;
+  mapping (address => uint) ownerPixelCount;
+
+  //checks that the user owns the pixel to be changed
+  modifier onlyOwnerOf(uint _pixelNum){
+  	require(msg.sender == pixelToOwner[_pixelNum]);
+  	_;
+  }
+  
+  //change the color of the pixel using _pixelNum as the array location
+  /*!! Need to ensure that each array item stays in the same order when they're modified
+    so that we can just store an array of colors and use the order in the array as the pixel location*/
+  function setColor(uint32 _pixelNum, bytes4 _color) public onlyOwnerOf{
+  	picture[_pixelNum].color = _color;
+  }
+  
+  for(i=0, i< 999999999, i++){
+  	setColor(i, 0);
+  }
 }
